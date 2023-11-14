08b96e3898d83ed073e99191e3c163331520e44e
kimatron <kimmykim@gmail.com>
2023-11-14 01:28:54 +0000

    Add temp hamburger navbar for smaller screen, adjust card size for smaller screen

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/** Function for hamburger menu on smaller screens */
+function toggleMenu() {
+  var menu = document.querySelector('.nav-list');
+  menu.classList.toggle('show-menu');
+}
+
 // listing vars here so they're in the global scope
 var cards, nCards, cover, openContent, openContentText, pageIsOpen = false,
   openContentImage, closeContent, windowWidth, windowHeight, currentCard;
@@ -182,3 +188,19 @@ function resize() {
 }
 
 var paragraphText = '<p>Somebody once told me the world is gonna roll me. I ain\'t the sharpest tool in the shed. She was looking kind of dumb with her finger and her thumb in the shape of an "L" on her forehead. Well the years start coming and they don\'t stop coming. Fed to the rules and I hit the ground running. Didn\'t make sense not to live for fun. Your brain gets smart but your head gets dumb. So much to do, so much to see. So what\'s wrong with taking the back streets? You\'ll never know if you don\'t go. You\'ll never shine if you don\'t glow.</p><p>Hey now, you\'re an all-star, get your game on, go play. Hey now, you\'re a rock star, get the show on, get paid. And all that glitters is gold. Only shooting stars break the mold.</p><p>It\'s a cool place and they say it gets colder. You\'re bundled up now, wait till you get older. But the meteor men beg to differ. Judging by the hole in the satellite picture. The ice we skate is getting pretty thin. The water\'s getting warm so you might as well swim. My world\'s on fire, how about yours? That\'s the way I like it and I never get bored.</p>';
+
+/**
+ * Intro type effect
+ */
+const typed = select('.typed')
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items')
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
